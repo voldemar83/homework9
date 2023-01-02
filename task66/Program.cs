cce83f916096b35d3ed91ea07bf7ff5f985e9035
Vladimir83 <vovaf@bk.ru>
2023-01-02 19:44:07 +0300

    task66

---
+++ task66/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите M: ");
+var str = Console.ReadLine() ?? "";
+var M = int.Parse(str);
+Console.Write("Введите N: ");
+str = Console.ReadLine() ?? "";
+var N = int.Parse(str);
+
+if (M > N)
+{
+    Console.WriteLine("Начальное число промежутка больше конечного.");
+    return;
+}
+
+var sum = 0;
+
+for (int i = M; i <= N; i++)
+    sum += i;
+
+Console.WriteLine($"Сумма натуральных чисел в промежуте от {M} до {N} равна: {sum}");
