8f7d2e11372bf95e251978215f53abd191c23de2
Vladimir83 <vovaf@bk.ru>
2023-01-02 19:39:15 +0300

    task64

---
+++ task64/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите N: ");
+var str = Console.ReadLine() ?? "";
+
+var N = int.Parse(str);
+
+static void DownFrom(int n)
+{
+    if (n == 0) return;
+    Console.Write($"{n--}" + (n != 0 ? ", " : ""));
+    DownFrom(n);
+}
+
+DownFrom(N);
